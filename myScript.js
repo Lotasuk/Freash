dd946a184deaa44f9d1eccd49c17a1c4aefa8ec4
Максим <92375339+Lotasuk@users.noreply.github.com>
2022-05-14 19:26:57 +0700

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,1 @@
+<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A0f76f74d837418a93b5875b77cf0f0597381b9da863de644da9e6378946d6d0a&amp;width=500&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
